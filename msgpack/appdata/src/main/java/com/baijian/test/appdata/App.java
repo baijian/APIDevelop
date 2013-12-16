9898efc5ef0a785b8f7153494ea121b305e67ded
柏健 <jian.baij@gmail.com>
2013-12-16 18:59:00 +0800

    init java demo project

---
+++ msgpack/appdata/src/main/java/com/baijian/test/appdata/App.java
@@ -0,0 +1,13 @@
+package com.baijian.test.appdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
